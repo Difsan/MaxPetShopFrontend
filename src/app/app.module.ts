1bce14e74d44d33530a06f546af65feb82c2b005
Difsan <difsanchezru@gmail.com>
2023-04-10 17:12:25 -0500

    components were created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { CardsComponent } from './cards/cards.component';
+import { ListsComponent } from './lists/lists.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    CardsComponent,
+    ListsComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
